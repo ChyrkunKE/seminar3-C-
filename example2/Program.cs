b9d698ca5d71b0774f9941b8ea656d3909ac3d51
Katsiara <katsiara@mail.ru>
2023-08-22 21:04:24 +0300

    Обратная задача

---
+++ example2/Program.cs
@@ -0,0 +1,21 @@
+﻿// //   Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+
+
+string QuarterRange(int quarter)
+{
+    if (quarter == 1) return "x > 0; y > 0";
+    if (quarter == 2) return "x < 0; y > 0";
+    if (quarter == 3) return "x < 0; y < 0";
+    if (quarter == 4) return "x > 0; y < 0";
+    return null;
+}
+
+Console.WriteLine ("Введите номер четверти:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string result= QuarterRange(number);
+
+Console.WriteLine (result != null ? result : "Некорректный ввод");
