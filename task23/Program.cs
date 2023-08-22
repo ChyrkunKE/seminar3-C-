59e29aa39d65ace497df6f7b5dc384543dbf8c65
Katsiara <katsiara@mail.ru>
2023-08-23 01:01:04 +0300

    Кубы

---
+++ task23/Program.cs
@@ -0,0 +1,17 @@
+﻿// принимает на вход число (N) 
+//  выдаёт таблицу кубов чисел от 1 до N
+
+
+void WriteTableCube (int number)
+{
+    int i = 1;
+    while (i <= number)
+    {
+        Console.WriteLine($"{i,5}-> {i*i*i,5}");
+        i= i+1;
+    }
+}
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+WriteTableCube (n);
